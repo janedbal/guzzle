41ad207b0dd1db7fb9b17c1a7d758bb35036b6ec
Jan Nedbal <janedbal@gmail.com>
2018-05-11 13:40:17 +0200

    GuzzleException extends Throwable

--- src/Exception/GuzzleException.php
+++ src/Exception/GuzzleException.php
@@ -1,13 +1,6 @@
 <?php
 namespace GuzzleHttp\Exception;
 
-/**
- * @method string getMessage()
- * @method \Throwable|null getPrevious()
- * @method mixed getCode()
- * @method string getFile()
- * @method int getLine()
- * @method array getTrace()
- * @method string getTraceAsString()
- */
-interface GuzzleException {}
+use Throwable;
+
+interface GuzzleException extends Throwable {}
